c7598f8eb94c700f5599900375b9bbac7fc74b2f
floatingFaez <floatingfaez@live.com>
2014-09-29 14:20:45 +0600

    Console Project Uploaded

---
+++ array/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace array
+{
+    class Person
+    {
+        public string firstName { get; private set; }
+        public int lastName { get; private set; }
+
+        public string sayHello(string nam)
+        {
+            return "Hello there " + nam;
+        }
+
+        #region To Show Array
+        //public string sayHello(params string[] names)
+        //{
+        //    return sayHello(string.Join(", ", names));
+        //} 
+        #endregion
+
+        public string sayHello(params string[] names) {
+            StringBuilder strBild = new StringBuilder();
+            string tmp = string.Empty;
+            foreach (string name in names)
+            {
+                strBild.Append(name + ", ");
+            }
+            tmp = strBild.ToString();
+            tmp = tmp.Remove(tmp.LastIndexOf(","));
+            return sayHello(tmp);     
+        }
+    }
+}
---
+++ array/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = new string[] { "Abul", "Faez","Faiza","Badol","Rashed","Hosna"};
+
+            #region for array 
+            //for (int i = 0; i < name.Length; i++)
+            //{
+            //   Console.WriteLine(name[i]);
+            //} 
+            #endregion
+
+            #region foreach array
+            //foreach (string i in names)
+            //{
+            //    Console.Write(i);
+            //    Console.Write(", ");
+            //} 
+            #endregion
+
+            Person Faez = new Person();
+
+            Console.WriteLine(Faez.sayHello(names));
+            Console.WriteLine(Faez.sayHello("Faez","Faiza","Abul","Milon","Badol"));
+
+
+            Console.WriteLine(string.Empty);
+            string String = "My Name Is Abul Faez. Whats your Name, Huh?";
+            char[] chararray = String.ToCharArray();
+            Array.Reverse(chararray);
+            Console.WriteLine(chararray);
+
+            Console.ReadLine();
+
+        }
+    }
+}
